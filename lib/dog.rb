c7d97bc263bce613ebe6a10f65be0b9c0712d7e7
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-06-01 20:27:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,15 +59,9 @@ class Dog
       Dog.create(name: name, breed: breed)
     end
   end
-  #   def self.find_or_create_by(name:, album:)
-  #   song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
-  #   if !song.empty?
-  #     song_data = song[0]
-  #     song = Song.new(song_data[0], song_data[1], song_data[2])
-  #   else
-  #     song = self.create(name: name, album: album)
-  #   end
-  #   song
-  # end
+
+  def self.find_by_name(name)
+    
+  end
   
 end
